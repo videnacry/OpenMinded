62c39faf1d68702bfcc177cfb3e2f5e6f6e4d5a5
Christian Callau <ch.callau@gmail.com>
2020-09-22 12:06:16 +0200

    Users table add description field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name', 20)->unique();
             $table->string('email')->unique();
+            $table->string('description');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
