a32ed08b67ebfadaf56bf1b8e09c39913f34e0fb
beron <bleink@outlook.es>
2020-09-27 15:07:53 +0200

    method to get idsBySenderId

---
+++ app/Http/Controllers/FriendController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Friend;
+use Illuminate\Http\Request;
+
+class FriendController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Friend  $friend
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Friend $friend)
+    {
+        //
+    }
+
+    /**
+     * Gets friends ids by the user id
+     * @param int $id
+     */
+    public function getIdsBySenderId(int $id){
+        $friends = Friend::where('sender', $id)->get();
+        $friendsIds = [];
+        foreach($friends as $friend){
+            array_push($friendsIds, $friend->id);
+        }
+        return $friendsIds;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Friend  $friend
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Friend $friend)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Friend  $friend
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Friend $friend)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Friend  $friend
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Friend $friend)
+    {
+        //
+    }
+}
