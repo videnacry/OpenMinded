38ebf8a0a15b492e2bb57af154db3064effdb7a9
beron <bleink@outlook.es>
2020-09-27 11:24:09 +0200

    profile header, contains a searcher, and button for likes, friends and post

---
+++ resources/views/user/header.blade.php
@@ -0,0 +1,22 @@
+@section('header-style')
+@endsection
+
+@section('header-content')
+    <nav class="flex justify-between h-16 px-20 py-4">
+        <div class="flex">
+            <input class="my-1 rounded-full text-xs border-solid border-2 border-gray-400 p-1 w-48 flex flex-col bg-gray-100 placeholder-gray-500" placeholder="Specific publication ?">
+            <button class="shadow-outer my-1 ml-3 bg-pink-300 rounded-md border-blue-200 border-solid border-2 px-2">Search</button>
+        </div>
+        <div class="flex justify-end">
+            <button class="inline h-full mr-3 relative">
+                <img width="40px" src="{{asset('img/heart.png')}}"/>
+                <span class="text-gray-200 absolute top-1 left-0">Likes</span>
+            </button>
+            <button class="inline h-full mr-3 relative">
+                <img width="50px" src="{{asset('img/friends.png')}}"/>
+                <span class="text-gray-200 absolute top-2 left-0">Friends</span>
+            </button>
+            <button class="shadow-outer my-1 mr-10 bg-orange-300 rounded-md border-gray-200 border-solid border-2 px-2">Post</button>
+        </div>
+    </nav>
+@endsection
