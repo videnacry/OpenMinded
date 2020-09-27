bb2afcc1b75f167fa1bec8d2547a06087f79afa6
beron <bleink@outlook.es>
2020-09-27 15:11:59 +0200

    files creation

---
+++ app/Models/Friend.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Friend extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'sender',
+        'receiver',
+        'status'
+    ];
+    
+    /**
+     * Returns the user of an specific friendship
+     */
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+}
