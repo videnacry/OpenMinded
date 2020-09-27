89021effde48e9aca3f61fa51d63a45c110713a2
beron <bleink@outlook.es>
2020-09-27 15:10:58 +0200

    method user for relation

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'author',
+        'content'
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+}
